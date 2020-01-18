3256449cd1508d46ffb15b2d8d7f802e10bf40d9
Michael Kopsho <michael.kopsho@gmail.com>
2020-01-18 23:43:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var newArray = [];
-  for (let counter = 0; counter > 4; counter++) {
-    return `${musicians[counter]} + plays + ${instruments[counter]}`;
+  for (let counter = 0; counter < 4; counter++) {
+    newArray.push(`${musicians[counter]} plays ${instruments[counter]}`);
   }
+  return newArray;
 }
