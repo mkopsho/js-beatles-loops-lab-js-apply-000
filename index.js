18120d45eeb781efe3cbc6be678a88eef61c247f
Michael Kopsho <michael.kopsho@gmail.com>
2020-01-18 23:43:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = [];
+  for (let counter = 0; counter > 4; counter++) {
+    return `${musicians[counter]} + plays + ${instruments[counter]}`;
+  }
+}
